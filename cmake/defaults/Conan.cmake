5c254bec5528037eed92e05ee3c1d6030597dc6c
anika02 <anika.korabliova@gmail.com>
2021-05-01 16:19:05 +0300

    First commit - Qt database

--- cmake/defaults/Conan.cmake
+++
@@ -1,6 +0,0 @@
-if (ENABLE_CONAN)
-    include(cmake/extra/conan.cmake)
-    conan_cmake_run(CONANFILE dependencies/conanfile.txt
-                    BASIC_SETUP CMAKE_TARGETS
-                    BUILD missing)
-endif ()
